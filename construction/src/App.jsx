cc0e7b5784d29abaf05fb823046de8bcb5a62280
mcjolly_prince <trustgodmcjolly@gmail.com>
2025-12-12 11:43:30 +0100

    lir

--- construction/src/App.jsx
+++ construction/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserLayout from './Components/Layout/UserLayout';
+
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+        {/* User Layout */}
+        </Route>
+
+        <Route>
+          {/* Admin Layout */}
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App
