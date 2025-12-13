d0c780dc0089da695c7b5175901bee84b8174cd4
mcjolly_prince <trustgodmcjolly@gmail.com>
2025-12-13 08:20:05 +0100

    fg

--- construction/src/Components/Layout/Topbar.jsx
+++ construction/src/Components/Layout/Topbar.jsx
@@ -6,9 +6,9 @@ import { RiTwitterXLine } from "react-icons/ri"
 
 const Topbar = () => {
   return (
-    <div className='bg-[#ea2e0e] text-white'>
-        <div className='container mx-auto'>
-            <div className='flex items-center space-x-4'>
+    <div className='bg-rabbit text-white'>
+        <div className='container mx-auto relative flex justify-between items-center py-3'>
+            <div className=' hidden md:flex items-center space-x-4'>
                 <a href='#' className='hover:text-gray-300'>
                     <TbBrandMeta className='h-5 w-5'/>
                 </a>
@@ -19,11 +19,11 @@ const Topbar = () => {
                     <RiTwitterXLine className='h-5 w-5'/>
                 </a>
             </div>
-            <div className='text-sm text-center'>
+            <div className='flex-grow text-sm text-center'>
                 <span> We ship worldwide - Fast and reliable shipping!</span>
             </div>
-            <div className='text-sm'>
-                <a></a>
+            <div className='text-sm hidden md:block'>
+                <a href='tel: +1234567890' className='hover:text-gray-300'>+1 (234) 567-890</a>
             </div>
         </div>
     </div>
