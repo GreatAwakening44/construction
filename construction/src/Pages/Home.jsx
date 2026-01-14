d295119ffa4efa4e73759d470697fd426417f416
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-14 22:26:51 +0100

    ui

--- construction/src/Pages/Home.jsx
+++ construction/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Layout/Hero'
 import { TbGridGoldenratio } from 'react-icons/tb'
 import Gender from '../Components/Products/Gender'
 import NewArrival from '../Components/Products/NewArrival'
+import ProductDetails from '../Components/Products/ProductDetails'
 
 const Home = () => {
   return (
@@ -10,6 +11,12 @@ const Home = () => {
         <Hero />
         <Gender />
         <NewArrival />
+
+        {/* Best seller */}
+        <h2 className='text-3xl text-center font-bold mb-4'>
+          Best Seller
+        </h2>
+        <ProductDetails />
     </div>
   )
 }
