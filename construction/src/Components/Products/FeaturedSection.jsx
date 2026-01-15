46a4c2a27bd61f38da81a78d17f89542ce794647
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-15 19:39:02 +0100

    hui

--- construction/src/Components/Products/FeaturedSection.jsx
+++ construction/src/Components/Products/FeaturedSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { HiShoppingBag } from 'react-icons/hi'
+import { HiArrowCircleDown, HiOutlineCreditCard, HiShoppingBag } from 'react-icons/hi'
+import { HiArrowPathRoundedSquare } from 'react-icons/hi2'
 
 const FeaturedSection = () => {
   return (
@@ -13,6 +14,28 @@ const FeaturedSection = () => {
             <h4 className="tracking-tighter mb-2">FREE INTERNATIONAL SHIPPING</h4>
             <p className='text-gray-600 text-sm tracking-tighter'>
                 On all orders over $100.00
+            </p>
+        </div>
+
+        {/* Feature 2*/}
+        <div className="flex flex-col items-center">
+            <div className="p-4 rounded-full mb-4">
+                <HiArrowPathRoundedSquare className='text-xl'/>
+            </div>
+            <h4 className="tracking-tighter mb-2">45 DAYS RETURN</h4>
+            <p className='text-gray-600 text-sm tracking-tighter'>
+                Money back gaurantee
+            </p>
+        </div>
+
+        {/* Feature 3*/}
+        <div className="flex flex-col items-center">
+            <div className="p-4 rounded-full mb-4">
+                <HiOutlineCreditCard className='text-xl'/>
+            </div>
+            <h4 className="tracking-tighter mb-2">SECURE CHECKOUT</h4>
+            <p className='text-gray-600 text-sm tracking-tighter'>
+                100% secured checkout process
             </p>
         </div>
     </div>
