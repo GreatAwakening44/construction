b2dafc76cc2c785005903a8f4642623cdb64efa0
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-15 21:32:56 +0100

    huy

--- construction/src/App.jsx
+++ construction/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserLayout from './Components/Layout/UserLayout';
 import { Toaster } from 'sonner';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<UserLayout />}>
             <Route index element={<Home />}/>
+            <Route path="login" element={<Login />} />
             </Route>
 
             <Route>
