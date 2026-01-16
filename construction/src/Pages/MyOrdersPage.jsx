4e6627823fcdfb0198918a2ce20340bea528368e
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-16 17:11:31 +0100

    uity

--- construction/src/Pages/MyOrdersPage.jsx
+++ construction/src/Pages/MyOrdersPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 const MyOrdersPage = () => {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            const mockOrders = [
+                {
+                    _id: 12345,
+                    createAt: new Date(),
+                    shippingAddress: {city: "New York", country: "USA"},
+                    orderItems: [
+                        {
+                            name: "Product 1",
+                            image: "https://picsum.photos/500/500?random=1"
+                        },
+                    ],
+                    totalPrice: 100,
+                    isPaid: true,
+                },
+                {
+                    _id: 34564,
+                    createAt: new Date(),
+                    shippingAddress: {city: "Saint Morris", country: "Australia"},
+                    orderItems: [
+                        {
+                            name: "Product 1",
+                            image: "https://picsum.photos/500/500?random=2"
+                        },
+                    ],
+                    totalPrice: 100,
+                    isPaid: true,
+                },
+            ];
+
+            setOrders(mockOrders);
+        }, 1000);
+    }, []);
+
   return (
-    <div>
-         hello world
+    <div className='max-w-7xl mx-auto p-4 sm:p-6'>
+        <h2 className="text-xl sm:text-2xl font-bold mb-6">My Orders</h2>
     </div>
   )
 }
--- construction/src/Pages/Register.jsx
+++ construction/src/Pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import register from "../assets/register.webp"
+import register from "../assets/register.webp";
 
 const Register = () => {
     const [name, setName] = useState("")
