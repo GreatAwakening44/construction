830f4b0915629174d13a2fc721e2d35076fa6ebf
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-17 17:56:14 +0100

    Jerti

--- construction/src/Components/Cart/Checkout.jsx
+++ construction/src/Components/Cart/Checkout.jsx
@@ -1,5 +1,6 @@
 // This is the checkout page that shows you details of your products and kets you make payments.
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const cart = {
     products: [
@@ -22,7 +23,52 @@ const cart = {
 };
 
 export const Checkout = () => {
+    const navigate = useNavigate();
+    const [shippingAddress, setShippingAddress] = useState({
+        firstName: "",
+        lastName: "",
+        address: "",
+        city: "",
+        postalCode: "",
+        country: "",
+        phone: "",
+    });
+
   return (
-    <div>Checkout</div>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter'>
+        {/* This is the left section */}
+        <div className='bg-white rounded-lg p-6'>
+            <h2 className='text-2xl uppercase mb-6'>Checkout</h2>
+            <form>
+                <h3 className='text-lg mb-4'>Contact Details</h3>
+                <div className='mb-4'>
+                    <label className='block text-gray-700'>Email</label>
+                    <input 
+                    type='email'
+                    value="user@example.com"
+                    className='w-full p-2 border rounded'
+                    disabled/>
+                </div>
+
+                <h3 className='text-lg mb-4'>Delivery</h3>
+                <div className="mb-4 grid grid-cols-2 gap-4">
+                    <div>
+                        <label className='block text-gray-700'>First Name</label>
+                        <input
+                        type='text'
+                        value={shippingAddress.firstName}
+                        onChange={(e) => setShippingAddress({
+                            ...shippingAddress, 
+                            firstName: e.target.value,
+
+                        })
+                    }
+                        className='w-full p-2 border rounded'
+                        required/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
