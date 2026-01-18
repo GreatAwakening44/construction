9c73058679a9cbfb031c87c360abcdb603bc247a
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-18 14:57:24 +0100

    jytu

--- construction/src/Pages/MyOrdersPage.jsx
+++ construction/src/Pages/MyOrdersPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const MyOrdersPage = () => {
     const [orders, setOrders] = useState([]);
@@ -66,7 +67,14 @@ const MyOrdersPage = () => {
                                     alt={order.orderItems[0].name}
                                     className='w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg'/>
                                 </td>
-                                <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">#{order._id}</td>
+                                <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">
+                                    <Link 
+                                            to={`/order/${order._id}`}
+                                            className="hover:text-blue-600 hover:underline cursor-pointer"
+                                        >
+                                            #{order._id}
+                                        </Link>
+                                </td>
                                 <td className="py-2 px-2 sm:py-4 sm:px-4">
                                     {new Date(order.createdAt).toLocaleDateString()}{""}
                                     {new Date(order.createdAt).toLocaleTimeString()}
