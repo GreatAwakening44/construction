963b722c9ca1020b5ba629669e1a5e18be7a3386
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-18 13:40:02 +0100

    tred

--- construction/src/Pages/OrderConfirmationPage.jsx
+++ construction/src/Pages/OrderConfirmationPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// This page confirms the order of whatever is about to be checked out
 
 const checkout = {
     _id: "12323",
@@ -28,12 +29,29 @@ const checkout = {
         address: "123 Fashion Street",
         city: "New York",
         country: "USA",
-    }
-}
+    },
+};
 
 const OrderConfirmationPage = () => {
   return (
-    <div>OrderConfirmationPage</div>
+    <div className='max-w-4xl mx-auto p-6 bg-white'>
+        <h1 className='text-4xl font-bold text-center text-emerald-700 mb-8'>
+            Thank You For your order!
+        </h1>
+
+        {checkout && (
+            <div className='p-6 rounded-lg border'>
+                <div className='flex justify-between mb-20'>
+                    {/* Order Id and Date */}
+                    <div>
+                        <h2 className='text-xl font-semibold'>Order ID: {checkout._id}</h2>
+                        <p className='text-gray-500'>Order date: {new Date(checkout.createdAt)}</p>
+                    </div>
+                </div>
+            </div>
+        )}
+
+    </div>
   )
 }
 
