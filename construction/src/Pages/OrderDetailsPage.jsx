c6889a3d1ebf0cca9330db9a2a61784364190651
mcjolly_prince <trustgodmcjolly@gmail.com>
2026-01-19 18:49:55 +0100

    go

--- construction/src/Pages/OrderDetailsPage.jsx
+++ construction/src/Pages/OrderDetailsPage.jsx
@@ -25,19 +25,33 @@ const OrderDetailsPage = () => {
                     Image: "https://picsum.photos/150?random=1"
                 },
                 {
-                    productId: "1",
-                    name: "Jacket",
-                    price: 120,
-                    quantiy: 1,
-                    Image: "https://picsum.photos/150?random=1"
+                    productId: "2",
+                    name: "Boots",
+                    price: 150,
+                    quantiy: 2,
+                    Image: "https://picsum.photos/150?random=4"
                 },
             ],
         };
-    });
-    
-  return (
-    <div>
 
+        setOrderDetails(mockOrderDetails)
+    }, [id]);
+
+  return (
+    <div className='max-w-7xl mx-auto p-4 sm:p-6'>
+        <h2 className='text-2xl md:text-3xl font-bold mb-6'>Order Details</h2>
+        {!orderDetails 
+        ? (<p>No order details found</p>
+
+        ) : (<div className='p-4 sm:p-6 rounded-lg border'>
+            {/* Order Info */}
+            <div className='flex flex-col sm:flex-row justify-between mb-8'>
+                <div>
+                    <h3 className='text-lg md:text-xl font-semibold'>Order ID: #{orderDetails._id}</h3>
+                    
+                </div>
+            </div>
+        </div>)}
     </div>
   )
 }
